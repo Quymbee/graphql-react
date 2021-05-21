d69e9e557912c895afa0f923c156fb05f5547aac
Quymbee Chen <quymbee@mattchen.com>
2021-05-21 11:45:32 -0400

    add company type to schema

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 graphql with react [udemy tutorial](https://www.udemy.com/course/graphql-with-react-course/)
 
+### technologies
+
 json-server
 
 - quick setup server for development
 - runs on sep localhost 3000
+
+nodemon
+
+- `npm run dev` script
+- helps to refresh server on change
--- schema/schema.js
+++ schema/schema.js
@@ -2,12 +2,31 @@ const graphql = require("graphql");
 const axios = require("axios");
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+});
+
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        //function pulling from json-server of users in db.json
+        //from companies endpoint
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then((res) => res.data);
+      },
+    },
   },
 });
 
@@ -20,6 +39,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
         //function pulling from json-server of users in db.json
+        //from users endpoint
         return axios
           .get(`http://localhost:3000/users/${args.id}`)
           .then((resp) => resp.data);
