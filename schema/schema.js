9f0009d98f79cc9387d985acf7d3ff73780c8c7f
Quymbee Chen <quymbee@mattchen.com>
2021-05-21 15:20:00 -0400

    functionally connected company and user types

--- schema/schema.js
+++ schema/schema.js
@@ -1,33 +1,46 @@
 const graphql = require("graphql");
 const axios = require("axios");
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+} = graphql;
 
 const CompanyType = new GraphQLObjectType({
   name: "Company",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     description: { type: GraphQLString },
-  },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then((res) => res.data);
+      },
+    },
+  }),
 });
 
 const UserType = new GraphQLObjectType({
   name: "User",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
     company: {
       type: CompanyType,
       resolve(parentValue, args) {
-        //function pulling from json-server of users in db.json
-        //from companies endpoint
+        // queries for user's matching company info. nested.
         return axios
           .get(`http://localhost:3000/companies/${parentValue.companyId}`)
           .then((res) => res.data);
       },
     },
-  },
+  }),
 });
 
 // sort of like an entry point that lands on specific node (specific User)
@@ -42,6 +55,17 @@ const RootQuery = new GraphQLObjectType({
         //from users endpoint
         return axios
           .get(`http://localhost:3000/users/${args.id}`)
+          .then((resp) => resp.data);
+      },
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        //function pulling from json-server of users in db.json
+        //from companies endpoint
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
           .then((resp) => resp.data);
       },
     },
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const expressGraphQL = require('express-graphql').graphqlHTTP;
-const schema = require('./schema/schema');
+const express = require("express");
+const expressGraphQL = require("express-graphql").graphqlHTTP;
+const schema = require("./schema/schema");
 
 const app = express();
 
-app.use('/graphql', expressGraphQL({
+app.use(
+  "/graphql",
+  expressGraphQL({
     schema,
     graphiql: true,
-}))
+  })
+);
 
 app.listen(4000, () => {
-    console.log('listening');
-})
+  console.log("listening");
+});
