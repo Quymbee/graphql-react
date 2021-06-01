9f455f0391cce0da496de76eb9e33fe3df163a67
Quymbee Chen <quymbee@mattchen.com>
2021-06-01 11:47:10 -0400

    lyric create component

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { graphql } from "react-apollo";
 import fetchSong from "../queries/fetchSong";
+import LyricCreate from "./LyricCreate";
 
 export class SongDetail extends Component {
   render() {
-    console.log(this.props);
+    const { song } = this.props.data;
 
-    return (
-      <div>
-        <h3>Song Detail</h3>
+    return this.props.data.loading ? (
+      <div>Loading...</div>
+    ) : (
+      <div className="container">
+        <Link to="/">Back</Link>
+        <h3>{song.title}</h3>
+        <LyricCreate songId={this.props.match.params.id} />
       </div>
     );
   }
 }
 
-export default SongDetail;
+export default graphql(fetchSong, {
+  options: (props) => {
+    return { variables: { id: props.match.params.id } };
+  },
+})(SongDetail);
--- client/components/SongList.js
+++ client/components/SongList.js
@@ -20,7 +20,7 @@ class SongList extends Component {
       this.props.data.songs.map(({ id, title }) => {
         return (
           <li key={id} className="collection-item">
-            {title}
+            <Link to={`/songs/${id}`}>{title}</Link>
             <i className="material-icons" onClick={() => this.onSongDelete(id)}>
               delete
             </i>
